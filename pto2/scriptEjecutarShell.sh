2863b1201b5779a162ca1ab79237c95b8909fe4f
geroinfante <143758273+GeroInfante@users.noreply.github.com>
2024-11-04 15:53:33 -0300

    feat: script para ejecutar y compilar la shell

---
+++ pto2/scriptEjecutarShell.sh
@@ -0,0 +1,11 @@
+gcc mostrar_ayuda.c -o mostrar_ayuda
+gcc ls.c -o ls
+gcc mkdir.c -o mkdir
+gcc crear_archivo.c -o crear_archivo
+gcc rmdir.c -o rmdir
+gcc cat.c -o cat
+gcc chmod.c -o chmod
+gcc shell.c -o main
+echo "Se compilo la Shell con exito"
+echo "Ejecutando shell..."
+./main
