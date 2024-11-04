1708734744355b2ad3da432c7aef3590bc11d27c
geroinfante <143758273+GeroInfante@users.noreply.github.com>
2024-11-04 15:19:08 -0300

    feat: scripts para compilacion y ejecucion de pumpernic

---
+++ pto1/1_1/PumperConColadeMensajes/src/scriptEjecutar.sh
@@ -0,0 +1,11 @@
+gcc cliente.c -o cliente
+gcc empleadoHamburguesa.c -o empleadoHamburguesa
+gcc empleadoVegano.c -o empleadoVegano
+gcc empleadoPapas.c -o empleadoPapas
+gcc despachador.c -o despachador
+gcc PumperNic-ColaMensajes.c -o main
+gcc eliminadorCola.c -o rm
+
+echo "Compilación exitosa"
+echo "Ejecutando programa..."
+./main
---
+++ pto1/1_1/PumperConPipes/scriptEjecutarPipes.sh
@@ -0,0 +1,4 @@
+gcc pumperNicPipes.c -o mainPipes -lpthread
+echo "Compilacion correcta"
+echo "Ejecutando programa.."
+./mainPipes
