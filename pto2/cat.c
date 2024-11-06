e54dfa744965f6bc32a3f0e404de7695babd0abb
geroinfante <143758273+GeroInfante@users.noreply.github.com>
2024-11-05 21:23:30 -0300

    fix cat con SC

--- pto2/cat.c
+++ pto2/cat.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
-
 
 int main(int argc, char *argv[])
 {
     char * nombre = argv[1];
-    FILE *file = fopen(nombre, "r");
-    if (!file) {
+    int file = open(nombre, O_RDONLY);
+    if (file == -1) {
         perror("Error al abrir el archivo");
         exit(1);
     }
 
     char c;
-    while ((c = fgetc(file)) != EOF) {//Recorre todo el archivo de texto y lo imprime en pantalla.
-        putchar(c);
+    while (read(file, &c, 1) > 0) {//Recorre todo el archivo de texto y lo imprime en pantalla.
+        write(STDOUT_FILENO, &c, 1);
     }
-    putchar('\n');
-    fclose(file);
+    write(STDOUT_FILENO, "\n", 1);
+    close(file);
     return 0;
 }
